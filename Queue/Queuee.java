246753896b1cda9b6fed2ba10a77fcd26bd75213
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-20 01:17:44 +0530

    queue

--- Queue/Queuee.java
+++ Queue/Queuee.java
@@ -15,8 +15,47 @@ public class Queuee {
         public static boolean IsEmpty(){
             return rear==-1;
         }
+
+        public static void add(int data){
+            if(rear==size-1){
+                System.out.println("queue is full");
+            }
+            rear=rear+1;
+            arr[rear]=data;
+        }
+
+        public static int remove(){
+            if(IsEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            int front=arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        public static int peak(){
+            if(IsEmpty()){
+                System.out.println("stack empty");
+                return -1;
+            }
+            return arr[0];
+        }
     }
     public static void main (String[]args){
+        queue q= new queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
 
+        while(!q.IsEmpty()){
+            System.out.println(q.peak());
+            q.remove();
+        }
+        
     }
 }
