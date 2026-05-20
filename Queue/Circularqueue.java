c2cae15d5e824afeba7c4faa113e57591d9ace16
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-20 10:15:45 +0530

    implementation

---
+++ Queue/Circularqueue.java
@@ -0,0 +1,19 @@
+public class Circularqueue {
+
+    static class circuArrQue{
+        static int arr[];
+        static int size;
+        static int front;
+        static int rear;
+
+        circuArrQue(int n){
+            arr=new int[3];
+            size=n;
+            front = -1;
+            rear = -1;
+        }
+    }
+    public static void main(String[]args){
+
+    }
+}
