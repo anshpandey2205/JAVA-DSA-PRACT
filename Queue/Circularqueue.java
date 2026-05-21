473b0e6ddd89860cc312620dc2ae0be3a10a554d
anshpandey2205 <anshupandey2227@gmail.com>
2026-05-21 22:54:47 +0530

    circular

--- Queue/Circularqueue.java
+++ Queue/Circularqueue.java
@@ -12,6 +12,44 @@ public class Circularqueue {
             front = -1;
             rear = -1;
         }
+
+        public static boolean IsEmpty(){
+            return front==-1 && rear==-1;
+        }
+
+        public static boolean IsFull(){
+            return (rear+1)%size==front;
+        }
+
+        public static void add(int data){
+            if(IsFull()){
+                System.out.println("Queue is full");
+                return;
+            }
+
+            // add first element 
+            if(front==-1){
+                front =0;
+            }
+
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+
+        public static int remove(){
+            if(IsEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int result= arr[front];
+
+            if(front==rear){
+                front=rear=-1;
+            }else{
+                front=(front+1)%size;
+            }
+            return result;
+        }
     }
     public static void main(String[]args){
 
