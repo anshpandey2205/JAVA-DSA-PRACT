586829d2e28b459ef56f3b82d3f60ebab70680b3
anshpandey2205 <anshupandey2227@gmail.com>
2026-06-01 09:57:58 +0530

    Activity Selection

--- Greedy Algorithm/ActivitySelection.java
+++ Greedy Algorithm/ActivitySelection.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class ActivitySelection {
     public static void main(String[]args){
         int start[]={1,3,0,5,8,5};
-        int end[]={2,4,6,7,9,9};
+        int   end[]={2,4,6,7,9,9};
 
         int maxAct=0;
         ArrayList<Integer>ans=new ArrayList<>();
@@ -22,7 +22,7 @@ public class ActivitySelection {
          }
 
          System.out.println("Max activity = "+maxAct);
-         for(int i=0;i<=ans.size();i++){
+         for(int i=0;i<ans.size();i++){
             System.out.println("A"+ans.get(i)+" ");
          }
 
