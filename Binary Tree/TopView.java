70db33bd08fe614db8c5a5767d1d3372ee3a8d98
anshpandey2205 <anshupandey2227@gmail.com>
2026-06-11 10:43:55 +0530

    TopView

---
+++ Binary Tree/TopView.java
@@ -0,0 +1,26 @@
+public class TopView {
+
+        static class Node{
+            int data;
+            Node left,right;
+    
+            public Node(int data){
+                this.data=data;
+                this.left=null;
+                this.right=null;
+            }
+        }
+
+        
+    public static void main(String args[]){
+        Node root=new Node(1);
+        root.left= new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+
+        topView(root);
+    }
+}
